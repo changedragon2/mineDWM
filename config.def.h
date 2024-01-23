4dee008fc1819be6d2965decf6582393e27b2003
changedragon2 <1301792688@qq.com>
2024-01-23 15:39:05 +0800

    给状态栏添加透明度，可以更改状态栏各区域颜色

--- config.def.h
+++ config.def.h
@@ -11,11 +11,39 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#005577";    /* similar to blue */
+static const char col_olive[]       = "#A3A700";    /* similar to olive green */
+static const char col_chartreuse[]  = "#D7FF00";    /* similar to yellow green */
+static const char col_orange[]      = "#FA7C04";    /* similar to orange */
+static const unsigned int baralpha        = 0xd0;
+static const unsigned int borderalpha     = OPAQUE;
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*                    fg              bg                border   */
+	[SchemeNorm]      = { col_gray3,      col_gray1,        col_gray2 },
+	[SchemeSel]       = { col_gray4,      col_cyan,         col_cyan  },
+  [SchemeTagsNorm]  = { col_gray3,      col_gray1,        col_gray2 },
+  [SchemeTagsSel]   = { col_gray4,      col_olive,        col_chartreuse },
+  [SchemeTitleNorm] = { col_gray3,      col_gray1,        col_gray2 },
+  [SchemeTitleSel]  = { col_gray4,      col_orange,       col_orange },
+  [SchemeStatus]    = { col_gray3,      col_gray1,        col_gray2 },
+};
+static const unsigned int alphas[][3] = {
+  /*                    fg              bg                border    */
+  [SchemeNorm]      = { OPAQUE,         baralpha,         borderalpha },
+  [SchemeSel]       = { OPAQUE,         baralpha,         borderalpha },
+  [SchemeTagsNorm]  = { OPAQUE,         baralpha,         borderalpha },
+  [SchemeTagsSel]   = { OPAQUE,         baralpha,         borderalpha },
+  [SchemeTitleNorm] = { OPAQUE,         baralpha,         borderalpha },
+  [SchemeTitleSel]  = { OPAQUE,         baralpha,         borderalpha },
+  [SchemeStatus]    = { OPAQUE,         baralpha,         borderalpha },
+  /* [SchemeStatus]    = { col_gray4,  col_gray1,  col_gray2   }, */
+  /* [SchemeTitleNorm] = { OPAQUE, baralpha, borderalpha }, */
+  /* [SchemeTitleSel]  = { OPAQUE, baralpha, borderalpha }, */
+  /* [SchemeTagsNorm]  = { OPAQUE, baralpha, borderalpha }, */
+  /* [SchemeTagsSel]   = { OPAQUE, baralpha, borderalpha }, */
+  /* [SchemeHidNorm]   = { OPAQUE, baralpha, borderalpha }, */
+  /* [SchemeHidSel]    = { OPAQUE, baralpha, borderalpha }, */
+  /* [SchemeUrg]       = { OPAQUE, baralpha, borderalpha }, */
 };
 
 /* tagging */
--- dwm.c
+++ dwm.c
@@ -57,9 +57,10 @@
 #define TAGMASK                 ((1 << LENGTH(tags)) - 1)
 #define TEXTW(X)                (drw_fontset_getwidth(drw, (X)) + lrpad)
 
+#define OPAQUE                  0xffU
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
-enum { SchemeNorm, SchemeSel }; /* color schemes */
+enum { SchemeNorm, SchemeSel, SchemeTagsNorm, SchemeTagsSel, SchemeTitleNorm, SchemeTitleSel, SchemeStatus }; /* color schemes */
 enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
        NetWMFullscreen, NetActiveWindow, NetWMWindowType,
        NetWMWindowTypeDialog, NetClientList, NetLast }; /* EWMH atoms */
@@ -239,6 +240,7 @@ static Monitor *wintomon(Window w);
 static int xerror(Display *dpy, XErrorEvent *ee);
 static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
+static void xinitvisual();
 static void zoom(const Arg *arg);
 
 /* variables */
@@ -274,6 +276,11 @@ static Display *dpy;
 static Drw *drw;
 static Monitor *mons, *selmon;
 static Window root, wmcheckwin;
+
+static int useargb = 0;
+static Visual *visual;
+static int depth;
+static Colormap cmap;
 
 /* configuration, allows nested code to access above variables */
 #include "config.h"
@@ -652,10 +659,10 @@ cyclelayout(const Arg *arg)
 {
   int i;
   int numlts = LENGTH(layouts);
-  for (i = 0; i < numtag && 
+  for (i = 0; i < numlts && 
       selmon->pertag->taglts[selmon->pertag->curtag][selmon->sellt] != &layouts[i]; i++)
     ;
-  i = (i + arg->i + numtag) % numlts;
+  i = (i + arg->i + numlts) % numlts;
   selmon->pertag->taglts[selmon->pertag->curtag][selmon->sellt] = &layouts[i];
   selmon->lt[selmon->sellt] = selmon->pertag->taglts[selmon->pertag->curtag][selmon->sellt];
   strncpy(selmon->ltsymbol, selmon->lt[selmon->sellt]->symbol, sizeof selmon->ltsymbol);
@@ -765,7 +772,7 @@ drawbar(Monitor *m)
 
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
-		drw_setscheme(drw, scheme[SchemeNorm]);
+		drw_setscheme(drw, scheme[SchemeStatus]);
 		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
 		drw_text(drw, m->ww - tw, 0, tw, bh, 0, stext, 0);
 	}
@@ -778,7 +785,7 @@ drawbar(Monitor *m)
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
 		w = TEXTW(tags[i]);
-		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
+		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeTagsSel : SchemeTagsNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		if (occ & 1 << i)
 			drw_rect(drw, x + boxs, boxs, boxw, boxw,
@@ -787,17 +794,17 @@ drawbar(Monitor *m)
 		x += w;
 	}
 	w = TEXTW(m->ltsymbol);
-	drw_setscheme(drw, scheme[SchemeNorm]);
+	drw_setscheme(drw, scheme[SchemeTagsNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
-			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
+			drw_setscheme(drw, scheme[m == selmon ? SchemeTitleSel : SchemeTitleNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
-			drw_setscheme(drw, scheme[SchemeNorm]);
+			drw_setscheme(drw, scheme[SchemeTitleNorm]);
 			drw_rect(drw, x, 0, w, bh, 1, 1);
 		}
 	}
@@ -1653,7 +1660,8 @@ setup(void)
 	sw = DisplayWidth(dpy, screen);
 	sh = DisplayHeight(dpy, screen);
 	root = RootWindow(dpy, screen);
-	drw = drw_create(dpy, screen, root, sw, sh);
+  xinitvisual();
+	drw = drw_create(dpy, screen, root, sw, sh, visual, depth, cmap);
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
@@ -1681,7 +1689,7 @@ setup(void)
 	/* init appearance */
 	scheme = ecalloc(LENGTH(colors), sizeof(Clr *));
 	for (i = 0; i < LENGTH(colors); i++)
-		scheme[i] = drw_scm_create(drw, colors[i], 3);
+		scheme[i] = drw_scm_create(drw, colors[i], alphas[i], 3);
 	/* init bars */
 	updatebars();
 	updatestatus();
@@ -1954,16 +1962,18 @@ updatebars(void)
 	Monitor *m;
 	XSetWindowAttributes wa = {
 		.override_redirect = True,
-		.background_pixmap = ParentRelative,
+    .background_pixel = 0,
+    .border_pixel = 0,
+    .colormap = cmap,
 		.event_mask = ButtonPressMask|ExposureMask
 	};
 	XClassHint ch = {"dwm", "dwm"};
 	for (m = mons; m; m = m->next) {
 		if (m->barwin)
 			continue;
-		m->barwin = XCreateWindow(dpy, root, m->wx, m->by, m->ww, bh, 0, DefaultDepth(dpy, screen),
-				CopyFromParent, DefaultVisual(dpy, screen),
-				CWOverrideRedirect|CWBackPixmap|CWEventMask, &wa);
+		m->barwin = XCreateWindow(dpy, root, m->wx, m->by, m->ww, bh, 0, depth,
+        InputOutput, visual,
+				CWOverrideRedirect|CWBackPixel|CWBorderPixel|CWColormap|CWEventMask, &wa);
 		XDefineCursor(dpy, m->barwin, cursor[CurNormal]->cursor);
 		XMapRaised(dpy, m->barwin);
 		XSetClassHint(dpy, m->barwin, &ch);
@@ -2275,6 +2285,43 @@ xerrorstart(Display *dpy, XErrorEvent *ee)
 {
 	die("dwm: another window manager is already running");
 	return -1;
+}
+
+void
+xinitvisual()
+{
+  XVisualInfo *infos;
+  XRenderPictFormat *fmt;
+  int nitems;
+  int i;
+
+  XVisualInfo tpl = {
+    .screen = screen,
+    .depth = 32,
+    .class = TrueColor
+  };
+  long masks = VisualScreenMask | VisualDepthMask | VisualClassMask;
+
+  infos = XGetVisualInfo(dpy, masks, &tpl, &nitems);
+  visual = NULL;
+  for (i = 0; i < nitems; i++){
+    fmt = XRenderFindVisualFormat(dpy, infos[i].visual);
+    if (fmt->type == PictTypeDirect && fmt->direct.alphaMask){
+      visual = infos[i].visual;
+      depth = infos[i].depth;
+      cmap = XCreateColormap(dpy, root, visual, AllocNone);
+      useargb = 1;
+      break;
+    }
+  }
+
+  XFree(infos);
+
+  if (!visual){
+    visual = DefaultVisual(dpy, screen);
+    depth = DefaultDepth(dpy, screen);
+    cmap = DefaultColormap(dpy, screen);
+  }
 }
 
 void
